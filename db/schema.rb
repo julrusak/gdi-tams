8dfea9dd997b040dde6e0d0c3fe016d7f741f680
Liz Abinante <eabinante@gmail.com>
2014-08-09 13:29:21 -0500

    catch up to stable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713153339) do
+ActiveRecord::Schema.define(version: 20140726222415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,13 @@ ActiveRecord::Schema.define(version: 20140713153339) do
 
   create_table "statuses", force: true do |t|
     t.string   "label"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teachers", force: true do |t|
+    t.integer  "course_id"
+    t.integer  "teaching_assistant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
